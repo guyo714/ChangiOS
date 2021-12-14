b545f2a3649dadaa53b2b9e633b1cc93efbbfd0d
changclublabs <Hu.YueChang@clublabs.com>
2021-12-14 10:30:38 -0800

    Initial Commit

---
+++ ChangiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChangiOS
+//
+//  Created by E700710 on 12/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
