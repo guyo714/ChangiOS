37084bc778df17c4d0dd4f1db784ec67d233000b
Shruti Sharma and Tony Jeng <pair+shruti+tony@aaa-calif.com>
2021-12-16 16:30:08 -0800

    Adds sample test.

---
+++ ChangiOSTests/ChangiOSTests.swift
@@ -0,0 +1,27 @@
+//
+//  ChangiOSTests.swift
+//  ChangiOSTests
+//
+//  Created by E700735 on 12/16/21.
+//
+
+import XCTest
+
+class ChangiOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let i = 0
+        XCTAssert(i == 0, "i is supposed to be 0")
+    }
+
+}
